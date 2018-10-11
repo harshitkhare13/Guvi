537cd4b1bfe8c747e2291c70ffa711cf0e526c7a
Harshit Khare <31098179+harshitkhare13@users.noreply.github.com>
2018-10-11 20:41:36 +0530

    Update PrimeNums.py

--- Set 2/PrimeNums.py
+++ Set 2/PrimeNums.py
@@ -1,6 +1,6 @@
 interval = list(map(int, input('Enter two numbers as interval: ').split()))
-count = 0
 for i in range(interval[0]+1, interval[1]):
+    count = 0
     for j in range(1,i+1):
         if i % j == 0:
             count += 1
